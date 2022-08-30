e878187497277a9ce48911db96b99c3ef059b6ff
Terrel Shumway <gh@terrel.codes>
2022-08-30 03:58:48 -0600

    make the second page work. with data from the url

--- 001-flask/program/routes.py
+++ 001-flask/program/routes.py
@@ -4,3 +4,9 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', user=name)
